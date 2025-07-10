211163c840389cc2ad7d750f344e08fe41caff38
Daniela <danielalinodasilvaoliveira@gmail.com>
2025-07-10 17:55:03 -0300

    chore: realizando as configurações iniciais

---
+++ todolist_app/src/app/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <div className="text-center underline">
+      <h1>TO DO LIST</h1>
+       <button className="btn">Clique me</button>
+    </div>
+  );
+}
