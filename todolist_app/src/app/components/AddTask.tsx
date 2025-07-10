cf05f04e8f01284a353dde0b090bdd5f376dac45
Daniela <danielalinodasilvaoliveira@gmail.com>
2025-07-10 18:36:45 -0300

    feat: componente 'AddTask' para adicionar nova tarefa feito.

---
+++ todolist_app/src/app/components/AddTask.tsx
@@ -0,0 +1,11 @@
+import { PiPlusBold } from "react-icons/pi";
+export function AddTask() {
+    return (
+        <div>
+            <button className="btn btn-primary w-full">
+                ADICIONAR NOVA TAREFA
+            <PiPlusBold size={13} />
+            </button>
+        </div>
+    )
+}
--- todolist_app/src/app/page.tsx
+++ todolist_app/src/app/page.tsx
@@ -1,9 +1,12 @@
+import { AddTask } from "./components/AddTask";
 
 export default function Home() {
   return (
-    <div className="text-center underline">
-      <h1>TO DO LIST</h1>
-       <button className="btn">Clique me</button>
-    </div>
+    <main className="max-w-4xl mx-auto  mt-6">
+      <div className="text-center my-5 flex flex-col gap-4">
+        <h1 className="text-2xl font-bold">To-do List App</h1>
+        <AddTask/>
+      </div>
+    </main>
   );
 }
